d06a4ee31960e6edd26c78fa18490fe84cdce250
AdiDD <adi.deaconu21@gmail.com>
2021-11-13 04:04:02 +0200

    Add Product entity

---
+++ ReStore/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStore.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //LONG type works better for sql and with stripe
+        public long Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
