30096531f2df20fe50ce6c5ec1aeb1c6d09de895
AdiDD <adi.deaconu21@gmail.com>
2021-11-20 04:15:52 +0200

    Create AddItem method for Basket

--- ReStore/Entities/Basket.cs
+++ ReStore/Entities/Basket.cs
@@ -10,5 +10,21 @@ namespace ReStore.Entities
         public int Id { get; set; }
         public string BuyerId { get; set; }
         public List<BasketItem> Items { get; set; } = new();
+
+        public void AddItem(Product product, int quantity)
+        {
+            // If the product dosen't already exist in the basket, add it as a new BasketItem
+            if (Items.All(item => item.ProductId != product.Id))
+            {
+                Items.Add(new BasketItem { Product = product, Quantity = quantity }); 
+            }
+
+            //Else if the product already exists in the Basket, only adjust the quantity
+            var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+        }
     }
 }
