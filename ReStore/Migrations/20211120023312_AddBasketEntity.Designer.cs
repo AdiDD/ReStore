728885271a9bcc7a825a491538b522531330eb4a
AdiDD <adi.deaconu21@gmail.com>
2021-11-20 04:38:26 +0200

    Add Basket to StoreContext as db set and update database

--- ReStore/Migrations/20211120023312_AddBasketEntity.Designer.cs
+++ ReStore/Migrations/20211120023312_AddBasketEntity.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReStore.Data;
 
 namespace ReStore.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20211120023312_AddBasketEntity")]
+    partial class AddBasketEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
