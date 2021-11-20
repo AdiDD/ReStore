3f18e51137f00ac6eaf18f807e7d2d8473994544
AdiDD <adi.deaconu21@gmail.com>
2021-11-20 23:47:51 +0200

    Create BasketController

---
+++ ReStore/Controllers/BasketController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReStore.Data;
+using ReStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStore.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BasketController(StoreContext context)
+        {
+            _context = context;
+        }
+    }
+}
