0c8deecdeec14ece615deb5a0f4e01d559b7bc22
AdiDD <adi.deaconu21@gmail.com>
2021-11-21 14:20:00 +0200

    Add axios methods for basket

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -5,47 +6,58 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Avatar, CardHeader } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
 
 import { Product } from "../../app/models/product";
-import { Avatar, CardHeader } from '@mui/material';
+import agent from '../../app/api/agent';
 
 interface Props {
     product: Product;
 }
 
 const ProductCard = ({product}: Props) => {
-    return (
-        <Card>
-          <CardHeader 
-            avatar={
-                <Avatar sx={{bgcolor: "secondary.main"}}>
-                    {product.name.charAt(0).toUpperCase()}
-                </Avatar>
-            }
-            title={product.name}
-            titleTypographyProps={{
-                sx: {fontWeight: "bold", color: "primary.main"}
-            }}
-          />  
-          <CardMedia
-            sx={{height: 140, backgroundSize: "contain"}}
-            image={product.pictureUrl}
-            title={product.name}
-          />
-          <CardContent>
-            <Typography gutterBottom color="secondary" variant="h5">
-              ${(product.price / 100).toFixed(2)}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {product.brand} / {product.type}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Add to cart</Button>
-            <Button size="small" component={Link} to={`/catalog/${product.id}`}>View</Button>
-          </CardActions>
-        </Card>
-      );
+  const [loading, setLoading] = useState(false);
+  
+  const handleAddItem = (productId: number) => {
+    setLoading(true);
+    agent.Basket.addItem(productId)
+    .catch(e => console.log(e))
+    .finally(() => setLoading(false));
+  }
+
+  return (
+      <Card>
+        <CardHeader 
+          avatar={
+              <Avatar sx={{bgcolor: "secondary.main"}}>
+                  {product.name.charAt(0).toUpperCase()}
+              </Avatar>
+          }
+          title={product.name}
+          titleTypographyProps={{
+              sx: {fontWeight: "bold", color: "primary.main"}
+          }}
+        />  
+        <CardMedia
+          sx={{height: 140, backgroundSize: "contain"}}
+          image={product.pictureUrl}
+          title={product.name}
+        />
+        <CardContent>
+          <Typography gutterBottom color="secondary" variant="h5">
+            ${(product.price / 100).toFixed(2)}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {product.brand} / {product.type}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <LoadingButton loading={loading} onClick={() => handleAddItem(product.id)} size="small">Add to cart</LoadingButton>
+          <Button size="small" component={Link} to={`/catalog/${product.id}`}>View</Button>
+        </CardActions>
+      </Card>
+    );
 };
 
 export default ProductCard;
