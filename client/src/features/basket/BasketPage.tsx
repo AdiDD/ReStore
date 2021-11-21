530d520e7525fb27d697e767876d6ac72dfef64a
AdiDD <adi.deaconu21@gmail.com>
2021-11-21 23:13:35 +0200

    Add BasketPage content

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,5 +1,14 @@
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Delete } from "@mui/icons-material";
+
 import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Basket } from "../../app/models/basket";
@@ -17,10 +26,42 @@ const BasketPage = () => {
 
     if (loading) return <LoadingComponent message="Loading basket..." />
 
-    if (!basket) return <Typography variant="h3">Your basket is empty</Typography> 
+    if (!basket?.items) return <Typography variant="h3">Your basket is empty</Typography> 
 
     return (
-        <h1>BuyerId = {basket.buyerId}</h1>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} size="small">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Product</TableCell>
+                    <TableCell align="right">Price</TableCell>
+                    <TableCell align="right">Quantity</TableCell>
+                    <TableCell align="right">Subtotal</TableCell>
+                    <TableCell align="right"></TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {basket.items.map(item => (
+                    <TableRow
+                    key={item.productId}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                    <TableCell component="th" scope="row">
+                        {item.name}
+                    </TableCell>
+                    <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
+                    <TableCell align="right">{item.quantity}</TableCell>
+                    <TableCell align="right">${(item.price * item.quantity / 100).toFixed(2)}</TableCell>
+                    <TableCell align="right">
+                        <IconButton color="error">
+                            <Delete />
+                        </IconButton>
+                    </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
