8338f919e505368f1752c6c719a1aa593dc63cac
AdiDD <adi.deaconu21@gmail.com>
2021-11-22 04:02:52 +0200

    Create provisory checkout page without functionality

---
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const CheckoutPage = () => {
+    return (
+        <Typography variant="h3">
+            You need to be logged in to access this page.
+        </Typography>
+    )
+}
+
+export default CheckoutPage;
