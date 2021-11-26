7c432fc1e4387622ecaa97ea16173d9f695a78eb
AdiDD <adi.deaconu21@gmail.com>
2021-11-26 17:47:20 +0200

    Create sort products extension method

---
+++ ReStore/Extensions/ProductExtensions.cs
@@ -0,0 +1,25 @@
+﻿using ReStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStore.Extensions
+{
+    public static class ProductExtensions
+    {
+        public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy)
+        {
+            if (string.IsNullOrWhiteSpace(orderBy)) return query.OrderBy(p => p.Name);
+
+            query = orderBy switch
+            {
+                "price" => query.OrderBy(p => p.Price),
+                "priceDesc" => query.OrderByDescending(p => p.Price),
+                _ => query.OrderBy(p => p.Name)
+            };
+
+            return query;
+        }
+    }
+}
