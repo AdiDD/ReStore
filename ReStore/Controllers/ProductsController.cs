8db86cd108dc7035f270c5abad6e3596bb6a9777
AdiDD <adi.deaconu21@gmail.com>
2021-11-26 19:42:18 +0200

    Setup GetProducts() to use ProductParams, and return a PagedList with pagination metadata in response header

--- ReStore/Controllers/ProductsController.cs
+++ ReStore/Controllers/ProductsController.cs
@@ -3,8 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using ReStore.Data;
 using ReStore.Entities;
 using ReStore.Extensions;
+using ReStore.RequestHelpers;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ReStore.Controllers
@@ -19,15 +21,19 @@ namespace ReStore.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm, string brands, string types)
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
         {
             var query = _context.Products
-                .Sort(orderBy)
-                .Search(searchTerm)
-                .Filter(brands, types)
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
                 .AsQueryable();
 
-            return await query.ToListAsync();
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
         }
 
         [HttpGet("{id}")]
