fe06faccf8fd8b49c7ff0c1c9e18a56704021b5b
AdiDD <adi.deaconu21@gmail.com>
2021-11-26 20:32:53 +0200

    Get filters when accesing Catalog page

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,18 +1,25 @@
 import { useEffect } from "react";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFiltersAsync, fetchProductsAsync, productSelectors } from "./catalogSlice";
 
 import ProductList from "./ProductList";
 
 const Catalog = () => {
     const products = useAppSelector(productSelectors.selectAll);
-    const { productsLoaded, status } = useAppSelector(state => state.catalog);
+    const { productsLoaded, filtersLoaded, status } = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
+
+    // i used two useEffect twice to avoid double loading of products because filters were loading faster than products and the page would rerender and then the component would
+    // fetch the products again.
 
     useEffect(() => {
       if (!productsLoaded) dispatch(fetchProductsAsync());
     }, [productsLoaded, dispatch])
+
+    useEffect(() => {
+      if (!filtersLoaded) dispatch(fetchFiltersAsync());
+    }, [filtersLoaded, dispatch])
 
     if (status.includes("pending")) return <LoadingComponent message="Loading Products..." />
 
