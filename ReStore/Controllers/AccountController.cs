791bb2ad76b129f0670d895955f8677e346ff5a9
AdiDD <adi.deaconu21@gmail.com>
2021-11-28 00:55:40 +0200

    Create empty AccountController

---
+++ ReStore/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ReStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStore.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+    }
+}
