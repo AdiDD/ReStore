4c2c20e303c18fd6aa8bcdd188ad7d0242bc2198
AdiDD <adi.deaconu21@gmail.com>
2021-11-28 09:39:30 +0200

    Add login logic in Header

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 import { Box } from "@mui/system";
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../store/configureStore";
+import SignedInMenu from "./SignedInMenu";
 
 interface Props {
     darkMode: boolean;
@@ -35,6 +36,7 @@ const navStyles = {
 
 const Header = ({ darkMode, handleThemeChange } : Props) => {
     const { basket } = useAppSelector(state => state.basket);
+    const { user } = useAppSelector(state => state.account);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
     return (
@@ -64,18 +66,22 @@ const Header = ({ darkMode, handleThemeChange } : Props) => {
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
-                    <List sx={{display: "flex"}}>
-                        {rightLinks.map(({title, path}) => (
-                            <ListItem
-                                component={NavLink}
-                                to={path}
-                                key={path}
-                                sx={navStyles}
-                            >
-                                {title.toUpperCase()}
-                            </ListItem>
-                        ))}
-                    </List>
+                    {user ? 
+                    (<SignedInMenu />) 
+                    : (
+                        <List sx={{display: "flex"}}>
+                            {rightLinks.map(({title, path}) => (
+                                <ListItem
+                                    component={NavLink}
+                                    to={path}
+                                    key={path}
+                                    sx={navStyles}
+                                >
+                                    {title.toUpperCase()}
+                                </ListItem>
+                            ))}
+                        </List>
+                    ) }
                 </Box>
             </Toolbar>
         </AppBar>
