369308969ebe8dce516fb9416186ad986f9a3173
AdiDD <adi.deaconu21@gmail.com>
2021-12-02 15:21:06 +0200

    Create OrdersController

---
+++ ReStore/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReStore.Data;
+using ReStore.Entities.OrderAggregate;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStore.Controllers
+{
+    [Authorize]
+    public class OrdersController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public OrdersController(StoreContext context)
+        {
+            _context = context;
+        }
+    }
+}
