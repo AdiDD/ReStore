673dd28097e74521d37f477404feea8c71a23de8
AdiDD <adi.deaconu21@gmail.com>
2021-12-04 04:58:33 +0200

    Create OrderDto

---
+++ ReStore/DTOs/OrderDto.cs
@@ -0,0 +1,21 @@
+﻿using ReStore.Entities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStore.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public ShippingAddress ShippingAddress { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public List<OrderItemDto> OrderItems { get; set; }
+        public long Subtotal { get; set; }
+        public long DeliveryFee { get; set; }
+        public string OrderStatus { get; set; }
+        public long Total => Subtotal + DeliveryFee;
+    }
+}
