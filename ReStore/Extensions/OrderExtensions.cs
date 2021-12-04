2d0a4180931af6b15a32e980ed0af6cd7a48bf8d
AdiDD <adi.deaconu21@gmail.com>
2021-12-04 05:01:17 +0200

    Return OrderDto from GetOrder and GetOrders

---
+++ ReStore/Extensions/OrderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReStore.DTOs;
+using ReStore.Entities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStore.Extensions
+{
+    public static class OrderExtensions
+    {
+        public static IQueryable<OrderDto> ProjectOrderToOrderDto(this IQueryable<Order> query)
+        {
+            return query
+                .Select(order => new OrderDto
+                {
+                    Id = order.Id,
+                    BuyerId = order.BuyerId,
+                    OrderDate = order.OrderDate,
+                    ShippingAddress = order.ShippingAddress,
+                    DeliveryFee = order.DeliveryFee,
+                    Subtotal = order.Subtotal,
+                    OrderStatus = order.OrderStatus.ToString(),
+                    OrderItems = order.OrderItems.Select(item => new OrderItemDto
+                    {
+                        ProdutcId = item.ItemOrdered.ProductId,
+                        Name = item.ItemOrdered.Name,
+                        PictureUrl = item.ItemOrdered.PictureUrl,
+                        Price = item.Price,
+                        Quantity = item.Quantity
+                    }).ToList()
+                }).AsNoTracking();
+        }
+    }
+}
