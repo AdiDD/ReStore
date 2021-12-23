016a38d9bee58e3d6cd403f75df914d13aa31eb6
AdiDD <adi.deaconu21@gmail.com>
2021-12-23 07:21:18 +0200

    Setup project to use heroku config variables in production

--- ReStore/Startup.cs
+++ ReStore/Startup.cs
@@ -12,6 +12,7 @@ using ReStore.Data;
 using ReStore.Entities;
 using ReStore.Middleware;
 using ReStore.Services;
+using System;
 using System.Text;
 
 namespace ReStore
@@ -34,10 +35,41 @@ namespace ReStore
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ReStore", Version = "v1" });
             });
+
+
             services.AddDbContext<StoreContext>(options => 
             {
-                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                string connStr;
+
+                if (env == "Development")
+                {
+                    // Use connection string from file
+                    connStr = Configuration.GetConnectionString("DefaultConnection");
+                }
+                else
+                {
+                    // Use connection string provided at runtime by Heroku
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                    // Parse connection URL to connection string for Npgsql
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
+                }
+
+                options.UseNpgsql(connStr);
             });
+
             services.AddCors();
 
             services.AddIdentityCore<User>(options =>
